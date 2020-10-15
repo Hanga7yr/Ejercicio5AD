c77922893bb1687048d07de45313253aa6285bd5
Hanga7yr <37352386+Hanga7yr@users.noreply.github.com>
2020-10-15 16:27:10 +0200

    Creation of the definining function fo the class basedatos

--- src/basedatos/Basedatos.java
+++ src/basedatos/Basedatos.java
@@ -1,5 +1,11 @@
 package basedatos;
+import java.sql.*;
 
 public abstract class Basedatos {
-
+	//Function to get the next row of the table
+	public static ResultSet getNext() { return null;}
+	//Function to get the last row of the table
+	public static ResultSet getLast() { return null;}
+	//Function to get a determined row
+	public static ResultSet getRow(int i) { return null;}
 }
