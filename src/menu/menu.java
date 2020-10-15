d74ee0d0abe4d3dfe964eb8b7abd9969c6984270
CharlyLlobregat <72161198+CharlyLlobregat@users.noreply.github.com>
2020-10-15 16:48:32 +0200

    Update menu.java

--- src/menu/menu.java
+++ src/menu/menu.java
@@ -1,5 +1,33 @@
 package menu;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public abstract class menu {
 
+	public void menu() throws IOException {
+		Scanner teclado = new Scanner(System.in);
+
+		int opcion;
+	    do {
+	        System.out.println("");
+	
+	        opcion = teclado.nextInt();
+	        teclado.nextLine();
+	
+	        switch (opcion) {
+	            case 1:
+	                System.out.println("");
+	                break;
+	            case 0:
+	                System.out.println("HASTA LA PROXIMA AMIGO");
+	                break;
+	            default:
+	                System.out.println("!- OPCION INCORRECTA -¡");
+	                break;
+	        }
+	    } while (opcion != 0);
+	}
+	
 }
